0ef2e14248450cbcddeaa5ee6cce68bb02676e56
Mahad Ahmad <mahmad@gtek.solutions>
2023-12-02 19:22:31 +0500

    frontend env configuration and recoil added

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = () => {
+  const env = {
+    APP_URI: process.env.APP_URI,
+  };
+  return env, nextConfig;
+};
--- Frontend/src/config/ApolloClient.js
+++ Frontend/src/config/ApolloClient.js
@@ -6,7 +6,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  uri: process.env.APP_URI,
   cache: new InMemoryCache(),
 });
 
